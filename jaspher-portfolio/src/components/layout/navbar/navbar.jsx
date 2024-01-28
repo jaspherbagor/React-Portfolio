68847927909df8cab07e258d3928492c213a6b46
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-28 21:36:54 +0800

    customized the toggler icon

--- jaspher-portfolio/src/components/layout/navbar/navbar.jsx
+++ jaspher-portfolio/src/components/layout/navbar/navbar.jsx
@@ -3,11 +3,11 @@ import '../../css/navbar.css';
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg px-md-4 px-sm-4 px-2 py-1 bg-dark">
+      <nav className="navbar navbar-expand-lg px-md-4 px-sm-4 px-2 py-1 bg-black">
         <div className="container-fluid">
           <a className="navbar-brand fs-2 fw-bolder text-white" href="#">Jaspher.</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon text-white"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
