10322ffedf3ae34b821c7ee31969179fa6e5e770
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-28 20:49:19 +0800

    added fontweight and fontsize of the navigation items

--- jaspher-portfolio/src/components/layout/navbar/navbar.jsx
+++ jaspher-portfolio/src/components/layout/navbar/navbar.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary px-4">
+      <nav className="navbar navbar-expand-lg px-4 py-1">
         <div className="container-fluid">
-          <a className="navbar-brand display-5 fw-bolder" href="#">Jaspher.</a>
+          <a className="navbar-brand fs-2 fw-bolder" href="#">Jaspher.</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item me-3">
+              <li className="nav-item me-3 fw-semibold fs-5">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li className="nav-item me-3">
+              <li className="nav-item me-3 fw-semibold fs-5">
                 <a className="nav-link" href="#">About</a>
               </li>
-              <li className="nav-item me-3">
+              <li className="nav-item me-3 fw-semibold fs-5">
                 <a className="nav-link" href="#">Portfolio</a>
               </li>
-              <li className="nav-item me-3">
+              <li className="nav-item me-3 fw-semibold fs-5">
                 <a className="nav-link" href="#">Contact</a>
               </li>
             </ul>
