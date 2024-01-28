0ff5b9288c9f3d1fa045c4bf78f34630ee5a3d1c
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-28 17:18:28 +0800

    added a structure/boilerplate of the navigation bar and footer

--- jaspher-portfolio/src/App.js
+++ jaspher-portfolio/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
-
+import Navbar from './components/layout/navbar/navbar';
+import Footer from './components/layout/footer/footer';
 function App() {
   return (
-    <>
+    <div className="App">
       <BrowserRouter>
-        
+        <Navbar/>
+
+        <Footer/>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
--- jaspher-portfolio/src/components/layout/navbar/navbar.jsx
+++ jaspher-portfolio/src/components/layout/navbar/navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div>navbar</div>
+    <div>This is a navbar</div>
   )
 }
 
-export default navbar
+export default Navbar
