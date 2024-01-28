10bb12ab11ee22bad596aeb3dff0beaaf2b12316
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-28 21:17:37 +0800

    added a responsive value of the padding

--- jaspher-portfolio/src/components/layout/footer/footer.jsx
+++ jaspher-portfolio/src/components/layout/footer/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <div className="bg-black">
-        <p className="text-center text-white px-4 py-2">© Copyright 2024. Jaspher Bagor Portfolio</p>
+        <p className="text-center text-white px-md-4 px-sm-4 px-2 py-2">© Copyright 2024. Jaspher Bagor Portfolio</p>
       </div>
     </div>
   )
