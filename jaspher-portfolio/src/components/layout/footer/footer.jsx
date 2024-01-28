a0b48da925e913c570e017d325504205d62d4e20
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-28 21:16:28 +0800

    added a padding in the footer

--- jaspher-portfolio/src/components/layout/footer/footer.jsx
+++ jaspher-portfolio/src/components/layout/footer/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <div className="bg-black">
-        <p className="text-center text-white">© Copyright 2024. Jaspher Bagor Portfolio</p>
+        <p className="text-center text-white px-4 py-2">© Copyright 2024. Jaspher Bagor Portfolio</p>
       </div>
     </div>
   )
