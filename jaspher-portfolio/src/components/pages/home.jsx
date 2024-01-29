80031840e0f987a66636053f40c905d2354f24d5
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-29 11:01:32 +0800

    added a hero image

--- jaspher-portfolio/src/components/pages/home.jsx
+++ jaspher-portfolio/src/components/pages/home.jsx
@@ -3,8 +3,21 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-        <section>
-            
+        <section className="container-fluid px-md-4 px-sm-4 px-2 py-5">
+            <div className="row">
+                <div className="col-md-6 d-flex justify-content-center align-items-center">
+                  <div className="container">
+                    <h3>Hello, I'm Jaspher</h3>
+                    <h1 className="display-3 fw-bolder">FULL-STACK WEB DEVELOPER</h1>
+                  </div>
+                </div>
+                <div className="col-md-6 d-flex justify-content-center align-items-center">
+                  <div className="container">
+                    <img src="images/hero_image.svg" alt="" />
+                  </div>
+                  
+                </div>
+            </div>
         </section>
     </>
   )
