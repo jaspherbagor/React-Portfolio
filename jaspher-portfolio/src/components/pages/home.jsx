48a87c3a639523e7a9f009750348aff2485735b0
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-29 16:19:05 +0800

    updated the hero image and styles of the hero elements

--- jaspher-portfolio/src/components/pages/home.jsx
+++ jaspher-portfolio/src/components/pages/home.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-
+import React from 'react';
+import '../css/home.css';
 const Home = () => {
   return (
     <>
-        <section className="container-fluid px-md-4 px-sm-4 px-2 py-5">
-            <div className="row">
-                <div className="col-md-6 d-flex justify-content-center align-items-center">
-                  <div className="container">
-                    <h3>Hello, I'm Jaspher</h3>
-                    <h1 className="display-3 fw-bolder">FULL-STACK WEB DEVELOPER</h1>
-                  </div>
+        <section className="hero-section container-fluid px-md-4 px-sm-4 px-2 py-5">
+          <div className="row">
+              <div className="col-md-6 d-flex justify-content-end align-items-center">
+                <div className="container">
+                  <h3>Hello, I'm Jaspher Bagor</h3>
+                  <h1 className="display-3 fw-bolder">FULL-STACK</h1>
+                  <h1 className="display-3 fw-bolder">WEB DEVELOPER</h1>
+                  <button className="btn btn-warning fw-semibold px-2 py-3 hero-btn">Contact Me</button>
                 </div>
-                <div className="col-md-6 d-flex justify-content-center align-items-center">
-                  <div className="container">
-                    <img src="images/hero_image.svg" alt="" />
-                  </div>
-                  
-                </div>
-            </div>
+              </div>
+              <div className="col-md-6 d-flex justify-content-center align-items-center">
+                <img src="images/hero_image.svg" alt="hero-image" className="img-fluid" />  
+              </div>
+          </div>
         </section>
     </>
   )
