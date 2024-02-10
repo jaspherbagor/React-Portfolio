3276a0190821568779484cb4698957ac92a02ba6
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-10 20:35:18 +0800

    added a tagline in the hero section

--- jaspher-portfolio/src/components/pages/home.jsx
+++ jaspher-portfolio/src/components/pages/home.jsx
@@ -9,7 +9,8 @@ const Home = () => {
                 <div className="container">
                   <h3>Hello! </h3>
                   <h1 className="fw-bolder display-3">I'm <span className="text-warning">Jaspher Bagor</span></h1>
-                  <h4 className="fw-bolder my-4">A Full-Stack Web Developer</h4>
+                  <h3 className="fw-bolder mt-4">A Full-Stack Web Developer</h3>
+                  <h4 className="fw-semibold mt-3 mb-4">Passionate about crafting innovative web solutions.</h4>                 
                   <button className="btn btn-warning fw-semibold px-2 py-3 hero-btn">Contact Me</button>
                 </div>
               </div>
@@ -21,6 +22,12 @@ const Home = () => {
         <section className="container-fluid about-section px-md-4 px-sm-4 px-2 py-5">
           <div className="row">
             <h2 className="fw-bolder text-center">ABOUT ME</h2>
+            <div className="col-md-6 d-flex justify-content-center align-items-center">
+              <img src="" alt="" />
+            </div>
+            <div className="col-md-6">
+
+            </div>
           </div>
         </section>
     </>
