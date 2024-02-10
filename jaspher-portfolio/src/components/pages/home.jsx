14cc23156cb0d666c4006464359326ac3f4fb66e
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-10 20:45:27 +0800

    added a temporary structure of the about page

--- jaspher-portfolio/src/components/pages/home.jsx
+++ jaspher-portfolio/src/components/pages/home.jsx
@@ -5,18 +5,18 @@ const Home = () => {
     <>
         <section className="hero-section container-fluid px-md-4 px-sm-4 px-2 py-5">
           <div className="row">
-              <div className="col-md-6 d-flex justify-content-center align-items-center">
-                <div className="container">
-                  <h3>Hello! </h3>
-                  <h1 className="fw-bolder display-3">I'm <span className="text-warning">Jaspher Bagor</span></h1>
-                  <h3 className="fw-bolder mt-4">A Full-Stack Web Developer</h3>
-                  <h4 className="fw-semibold mt-3 mb-4">Passionate about crafting innovative web solutions.</h4>                 
-                  <button className="btn btn-warning fw-semibold px-2 py-3 hero-btn">Contact Me</button>
-                </div>
+            <div className="col-md-6 d-flex justify-content-center align-items-center">
+              <div className="container">
+                <h3>Hello! </h3>
+                <h1 className="fw-bolder display-3">I'm <span className="text-warning">Jaspher Bagor</span></h1>
+                <h3 className="fw-bolder mt-4">A Full-Stack Web Developer</h3>
+                <h4 className="fw-semibold mt-3 mb-4">Passionate about crafting innovative web solutions.</h4>                 
+                <button className="btn btn-warning fw-semibold px-2 py-3 hero-btn">Contact Me</button>
               </div>
-              <div className="col-md-6 d-flex justify-content-center align-items-center">
-                <img src="images/hero_image.svg" alt="hero-image" className="img-fluid" />  
-              </div>
+            </div>
+            <div className="col-md-6 d-flex justify-content-center align-items-center">
+              <img src="images/hero_image.svg" alt="hero-image" className="img-fluid" />  
+            </div>
           </div>
         </section>
         <section className="container-fluid about-section px-md-4 px-sm-4 px-2 py-5">
@@ -26,7 +26,9 @@ const Home = () => {
               <img src="" alt="" />
             </div>
             <div className="col-md-6">
-
+              <h4 className="mt-5">I am a Philippine-based full-stack web developer with a strong passion for creating dynamic and user-friendly applications.</h4>
+              <h5>My journey into web development started [mention a brief story]. I specialize in [mention technologies or languages], always eager to learn and adapt to new challenges.
+</h5>
             </div>
           </div>
         </section>
