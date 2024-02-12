dabaaea0f786ca4a9b2735df5c436ac92677c8f9
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-12 12:59:13 +0800

    added an icon to the project nav item

--- jaspher-portfolio/src/components/layout/navbar/navbar.jsx
+++ jaspher-portfolio/src/components/layout/navbar/navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                 <a className="nav-link text-white" href="#about"><i class="bi bi-person-circle fs-5"></i> About</a>
               </li>
               <li className="nav-item me-3 fs-5">
-                <a className="nav-link text-white" href="#projects">Projects</a>
+                <a className="nav-link text-white" href="#projects"><i class="bi bi-code-square fs-5"></i> Projects</a>
               </li>
               <li className="nav-item me-3 fs-5">
                 <a className="nav-link text-white" href="#">Contact</a>
