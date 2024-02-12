4bc06713d3699cd9ca9500cecd7f33a66ca61064
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-12 13:00:16 +0800

    updated the font-size of the icons

--- jaspher-portfolio/src/components/layout/navbar/navbar.jsx
+++ jaspher-portfolio/src/components/layout/navbar/navbar.jsx
@@ -12,13 +12,13 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item me-3 fs-5">
-                <a className="nav-link active text-white" aria-current="page" href="#"><i class="bi bi-house fs-5"></i> Home</a>
+                <a className="nav-link active text-white" aria-current="page" href="#"><i class="bi bi-house fs-4"></i> Home</a>
               </li>
               <li className="nav-item me-3 fs-5">
-                <a className="nav-link text-white" href="#about"><i class="bi bi-person-circle fs-5"></i> About</a>
+                <a className="nav-link text-white" href="#about"><i class="bi bi-person-circle fs-4"></i> About</a>
               </li>
               <li className="nav-item me-3 fs-5">
-                <a className="nav-link text-white" href="#projects"><i class="bi bi-code-square fs-5"></i> Projects</a>
+                <a className="nav-link text-white" href="#projects"><i class="bi bi-code-square fs-4"></i> Projects</a>
               </li>
               <li className="nav-item me-3 fs-5">
                 <a className="nav-link text-white" href="#">Contact</a>
