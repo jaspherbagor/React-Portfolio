e0f00aaed0a5ea4d185f8b0a22d3f858303211e0
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-13 10:03:59 +0800

    updated the link of my mini-project 2

--- jaspher-portfolio/src/components/pages/home.jsx
+++ jaspher-portfolio/src/components/pages/home.jsx
@@ -188,10 +188,10 @@ const Home = () => {
                   electrical tools, materials, and equipments.
                 </p>
                 <div className="d-flex align-items-center justify-content-center">
-                  <a href="#" className="btn btn-success live-preview-btn me-4">
+                  <a href="https://spark-source-central.vercel.app/" className="btn btn-success live-preview-btn me-4">
                     <i className="bi bi-zoom-in fs-5"></i> Live Preview
                   </a>
-                  <a href="#" className="btn btn-secondary github-code-btn">
+                  <a href="https://github.com/jaspherbagor/SparkSourceCentral" className="btn btn-secondary github-code-btn">
                     <i class="bi bi-github fs-5"></i> Github Code
                   </a>
                 </div>
