e9ce18ccb098994db1052bc48fe0e78156e9cc69
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-13 21:35:44 +0800

    make the live link and the github code of the capstone project open in the new tab

--- jaspher-portfolio/src/components/pages/home.jsx
+++ jaspher-portfolio/src/components/pages/home.jsx
@@ -214,10 +214,10 @@ const Home = () => {
                   job, subscribe to a plan and manage applicants.
                 </p>
                 <div className="d-flex align-items-center justify-content-center">
-                  <a href="https://splenr.tech/" className="btn btn-success live-preview-btn me-4">
+                  <a href="https://splenr.tech/" target="_blank" className="btn btn-success live-preview-btn me-4">
                     <i className="bi bi-zoom-in fs-5"></i> Live Preview
                   </a>
-                  <a href="https://github.com/jaspherbagor/SPLENR" className="btn btn-secondary github-code-btn">
+                  <a href="https://github.com/jaspherbagor/SPLENR" target="_blank" className="btn btn-secondary github-code-btn">
                     <i class="bi bi-github fs-5"></i> Github Code
                   </a>
                 </div>
