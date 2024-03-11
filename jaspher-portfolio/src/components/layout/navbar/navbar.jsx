fcae4699e2dd71677cdf198186b11363fad44913
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-11 16:11:50 +0800

    updated the case of the navlinks

--- jaspher-portfolio/src/components/layout/navbar/navbar.jsx
+++ jaspher-portfolio/src/components/layout/navbar/navbar.jsx
@@ -5,23 +5,23 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg px-md-4 px-sm-4 px-2 py-1 position-fixed container-fluid">
         <div className="container-fluid">
-          <a className="navbar-brand fs-2 fw-bolder text-white" href="/">jas<span>pher.</span></a>
+          <a className="navbar-brand fs-2 fw-medium text-white" href="/">jas<span>pher.</span></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item me-3 fs-5">
-                <a className="nav-link active text-white" aria-current="page" href="#"><i className="bi bi-house fs-5"></i> HOME</a>
+                <a className="nav-link active text-white" aria-current="page" href="#"><i className="bi bi-house fs-5"></i> Home</a>
               </li>
               <li className="nav-item me-3 fs-5">
-                <a className="nav-link text-white" href="#about"><i className="bi bi-person-circle fs-5"></i> ABOUT</a>
+                <a className="nav-link text-white" href="#about"><i className="bi bi-person-circle fs-5"></i> About</a>
               </li>
               <li className="nav-item me-3 fs-5">
-                <a className="nav-link text-white" href="#projects"><i className="bi bi-code-square fs-5"></i> PROJECTS</a>
+                <a className="nav-link text-white" href="#projects"><i className="bi bi-code-square fs-5"></i> Projects</a>
               </li>
               <li className="nav-item me-3 fs-5">
-                <a className="nav-link text-white" href="#contactMe"><i className="bi bi-telephone fs-5"></i> CONTACT</a>
+                <a className="nav-link text-white" href="#contactMe"><i className="bi bi-telephone fs-5"></i> Contact</a>
               </li>
             </ul>
           </div>
